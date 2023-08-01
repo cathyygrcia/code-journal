b4e9cf4aa6fb8556fcb82f7baa6cef19557b5cbb
Cathy Garcia <garciacathy67@yahoo.com>
2023-08-01 23:53:05 +0000

    Created event listener on document and looped through data, and appended DOM tree to unordered list.

--- js/main.js
+++ js/main.js
@@ -21,3 +21,40 @@ $entryForm.addEventListener('submit', function (event) {
   $entryImage.setAttribute('src', entryImagePlaceholder);
   $entryForm.reset();
 });
+
+function renderEntry(entry) {
+  const $entryItem = document.createElement('li');
+  $entryItem.setAttribute('class', 'row');
+
+  const $imageWrapper = document.createElement('div');
+  $imageWrapper.setAttribute('class', 'column-half');
+
+  const $image = document.createElement('img');
+  $image.setAttribute('src', entry.photoUrl);
+
+  const $entryWrapper = document.createElement('div');
+  $entryWrapper.setAttribute('class', 'column-half');
+
+  const $title = document.createElement('h2');
+  $title.textContent = entry.title;
+
+  const $notes = document.createElement('p');
+  $notes.textContent = entry.notes;
+
+  $entryItem.appendChild($imageWrapper);
+  $imageWrapper.appendChild($image);
+  $entryItem.appendChild($entryWrapper);
+  $entryWrapper.appendChild($title);
+  $entryWrapper.appendChild($notes);
+
+  return $entryItem;
+}
+
+const $journalEntriesList = document.querySelector('.journal-entries');
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  for (let i = 0; i < data.entries.length; i++) {
+    const $entry = renderEntry(data.entries[i]);
+    $journalEntriesList.appendChild($entry);
+  }
+});
